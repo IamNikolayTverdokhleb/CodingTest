e36147065659e535920de8a082ed2e2dfc9ab3b0
Nikolay Tverdokhleb <48504887+IamNikolayTverdokhleb@users.noreply.github.com>
2021-02-17 12:16:11 +0100

    Add improvement to comparison script
    Now it shows more detailed information of differences.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-compare -metric rmse imgs_transformed/kalray.png goldens/kalray.png /dev/null
-compare -metric rmse imgs_transformed/manycore-img.png goldens/manycore-img.png /dev/null
+compare -verbose -metric mae imgs_transformed/kalray.png goldens/kalray_Rx1.2_Gx0.8_Bx0.8.png /dev/null
+compare -verbose -metric mae imgs_transformed/manycore-img.png goldens/manycore-img.png /dev/null
